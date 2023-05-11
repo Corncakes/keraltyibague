6ff9214794212970edc61cfc3887ed8195530d95
Jorge Alvarado <g.orge.alvarado@gmail.com>
2023-05-11 05:33:18 -0500

    Diseño responsive de home, servicios, especialidades quirurgicas y especialistas

--- web/themes/custom/keralty_ibague_theme/js/custom.js
+++ web/themes/custom/keralty_ibague_theme/js/custom.js
@@ -20,15 +20,33 @@
           $(".form-buscar").hide(); 
         }
       });
-      
-      var slider = tns({ 
-        container: '.view-slider .view-content.row',
-        mode: 'carousel',
-        controlsPosition: 'bottom',
-        navPosition: 'bottom',
-        "mouseDrag": true,
-        controlsText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
-      });
+      if (document.querySelector('.page-node-1')){
+		var slider = tns({ 
+			container: '.view-slider .view-content.row',
+			mode: 'carousel',
+			controlsPosition: 'bottom',
+			navPosition: 'bottom',
+			"mouseDrag": true,
+			controlsText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+		});
+	  }
+	  var acc = document.querySelectorAll(".site-footer .block-menu");
+	  var i;
+
+for (i = 0; i < acc.length; i++) {
+	
+	acc[i].classList.toggle("inactive");
+  acc[i].querySelector("h2").addEventListener("click", function() {
+	  console.log(this.parentElement);
+    this.parentElement.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
     }
   }
 
